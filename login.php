1d22b70a8490533e8d53e19d795442e28d0f89d1
NhanDucc <121173933+NhanDucc@users.noreply.github.com>
2024-08-19 18:54:06 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,32 +1,20 @@
 <?php
 session_start();
 $error_message = '';
-include 'includes\DatabaseConnection.php';
+include 'includes/DatabaseConnection.php';
+include 'includes/DatabaseFunctions.php';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    $stmt = $pdo->prepare("SELECT * FROM account WHERE username = :username AND password = :password");
-    $stmt->bindValue(':username', $username);
-    $stmt->bindValue(':password', $password);
-    $stmt->execute();
-
-    if ($stmt->rowCount() > 0) {
+    if ($user = validateLogin($pdo, $username, $password)) {
         $_SESSION['username'] = $username;
         header('Location: index.php');
         exit;
     } else {
-        $error_message = "Invalid username or password.";
-        $_SESSION['error_message'] = $error_message;
+        $_SESSION['error'] = "Invalid username or password.";
         header("Location: login.php");
         exit;
     }
 }
-
-if(isset($_SESSION['error_message'])) {
-    $error_message = $_SESSION['error_message'];
-    unset($_SESSION['error_message']);
-}
-
 include 'templates\login.html.php';
